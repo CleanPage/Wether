8c077d99cbc5972bf3bb7819d8f50f286951a9bf
CleanPage <107153502+CleanPage@users.noreply.github.com>
2022-07-27 20:02:18 +0200

    Initial Commit

---
+++ Wether/WetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherApp.swift
+//  Wether
+//
+//  Created by Егор  on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
